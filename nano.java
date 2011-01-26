19fca59f6d919f143b651301efea7c5b6e0ab029
jdalton <john.david.dalton@gmail.com>
2011-01-26 00:43:17 -0500

    benchmark: Process jar with ProGuard to save an additional 6 bytes. [jddalton]

--- nano.java
+++ nano.java
@@ -1,8 +1,19 @@
-/** 
+/**
  * Simple class to expose nanoTime() to JavaScript.
- * Compile using:
+ *
+ * Compile using
  *   javac -g:none -target 1.5 nano.java
  *   jar cfM nano.jar nano.class
+ *   java -jar progaurd.jar @options.txt
+ *
+ * ProGuard (http://proguard.sourceforge.net)
+ * options.txt
+ *   -injars      nano.jar
+ *   -outjars     nano_s.jar
+ *   -libraryjars <java.home>/jre/lib/rt.jar
+ *   -keep public class nano {
+ *     public long nanoTime();
+ *   }
  */
 import java.applet.Applet;
 public class nano extends Applet {
