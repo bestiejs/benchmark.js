38c3493c1075b826e70bb8a5e4aefa8534755f7c
Mathias Bynens <mathias@qiwi.be>
2011-09-22 11:25:32 +0200

    Simplify the doc parser a bit.

--- docs/parse.php
+++ docs/parse.php
@@ -1,12 +1,12 @@
 <?php
 
   // cleanup requested filepath
-  $_GET['f'] = isset($_GET['f']) ? $_GET['f'] : 'benchmark';
-  $_GET['f'] = preg_replace('#(\.*[\/])+#', '', $_GET['f']);
-  $_GET['f'] = $_GET['f'] . (preg_match('/\.[a-z]+$/', $_GET['f']) ? '' : '.js');
+  $file = isset($_GET['f']) ? $_GET['f'] : 'benchmark';
+  $file = preg_replace('#(\.*[\/])+#', '', $file);
+  $file .= preg_match('/\.[a-z]+$/', $file) ? '' : '.js';
 
   // output filename
-  $_GET['o'] = isset($_GET['o']) ? $_GET['o'] : basename($_GET['f']);
+  $output = isset($_GET['o']) ? $_GET['o'] : basename($file);
 
   /*--------------------------------------------------------------------------*/
 
@@ -14,12 +14,12 @@
 
   // generate Markdown
   $markdown = docdown(array(
-    'path' => '../' . $_GET['f'],
+    'path' => '../' . $file,
     'url'  => 'https://github.com/bestiejs/benchmark.js/blob/master/benchmark.js'
   ));
 
   // save to a .md file
-  file_put_contents($_GET['o'] . '.md', $markdown);
+  file_put_contents($output . '.md', $markdown);
 
   // print
   header('Content-Type: text/plain;charset=utf-8');
