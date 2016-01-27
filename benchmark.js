2e10917508a0a90a7b40aebc200fc142283b77e6
John-David Dalton <john.david.dalton@gmail.com>
2016-01-27 07:16:57 -0800

    Use https for the platform.js url. [ci skip]

--- benchmark.js
+++ benchmark.js
@@ -2251,7 +2251,7 @@
 
       /**
        * Platform object with properties describing things like browser name,
-       * version, and operating system. See [`platform.js`](http://mths.be/platform).
+       * version, and operating system. See [`platform.js`](https://mths.be/platform).
        *
        * @static
        * @memberOf Benchmark
