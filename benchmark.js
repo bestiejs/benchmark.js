fb382b61427546b3f47b044fba67614f985a38cd
Kevin Locke <kevin@kevinlocke.name>
2016-07-26 06:58:47 -0700

    Fix Benchmark#toString() stringification of error. (#122)

--- benchmark.js
+++ benchmark.js
@@ -1530,8 +1530,18 @@
           result = bench.name || (_.isNaN(id) ? id : '<Test #' + id + '>');
 
       if (error) {
-        result += ': ' + join(error);
-      } else {
+        var errorStr;
+        if (!_.isObject(error)) {
+          errorStr = String(error);
+        } else if (!_.isError(Error)) {
+          errorStr = join(error);
+        } else {
+          // Error#name and Error#message properties are non-enumerable.
+          errorStr = join(_.assign({ 'name': error.name, 'message': error.message }, error));
+        }
+        result += ': ' + errorStr;
+      }
+      else {
         result += ' x ' + formatNumber(hz.toFixed(hz < 100 ? 2 : 0)) + ' ops/sec ' + pm +
           stats.rme.toFixed(2) + '% (' + size + ' run' + (size == 1 ? '' : 's') + ' sampled)';
       }
