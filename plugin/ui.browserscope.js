97c5ab7af175cfb8b7c0dc1ae3be4e0758a1ad2c
Benjamin Tan <demoneaux@gmail.com>
2016-10-22 21:07:46 +0800

    Fix issue on Chromium based browsers.
    See https://github.com/jsperf/jsperf.com/issues/235#issuecomment-254987353.
    
    Ref. jsperf/jsperf.com#235.

--- plugin/ui.browserscope.js
+++ plugin/ui.browserscope.js
@@ -1044,22 +1044,25 @@
     // the frame window of the charts
     me.chartWindow = iwin;
 
-    // the element the charts are inserted into
-    me.container = query('#bs-chart', idoc)[0];
+    // ensure that content in iframe is processed by executing on next tick
+    setTimeout(function() {
+      // the element the charts are inserted into
+      me.container = query('#bs-chart', idoc)[0];
 
-    // Browserscope's UA div is inserted before an element with the id of "bs-ua-script"
-    loadScript('https://www.browserscope.org/ua?o=js', me.container).id = 'bs-ua-script';
+      // Browserscope's UA div is inserted before an element with the id of "bs-ua-script"
+      loadScript('https://www.browserscope.org/ua?o=js', me.container).id = 'bs-ua-script';
 
-    // the "autoload" string is created following the guide at
-    // https://developers.google.com/loader/?hl=en#auto-loading
-    loadScript('https://www.google.com/jsapi?autoload=' + encodeURIComponent('{' +
-      'modules:[{' +
-        'name:"visualization",' +
-        'version:1,' +
-        'packages:["corechart","table"],' +
-        'callback:ui.browserscope.load' +
-      '}]' +
-    '}'), idoc);
+      // the "autoload" string is created following the guide at
+      // https://developers.google.com/loader/?hl=en#auto-loading
+      loadScript('https://www.google.com/jsapi?autoload=' + encodeURIComponent('{' +
+        'modules:[{' +
+          'name:"visualization",' +
+          'version:1,' +
+          'packages:["corechart","table"],' +
+          'callback:ui.browserscope.load' +
+        '}]' +
+      '}'), idoc);
+    }, 1);
   });
 
   // hide the chart while benchmarks are running
